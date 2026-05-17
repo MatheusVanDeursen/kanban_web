48ed5475ca46de02910c02ef800ac9a73cce44fe
MatheusVanDeursen <matheus.vandeursen@gmail.com>
2026-05-17 15:22:48 -0300

    URL HTTPS

--- scripts/reset-password.js
+++ scripts/reset-password.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://192.168.15.27:3000/api';
+const API_URL = 'https://api-kanban.matheusvandeursen.com/api';
 
 // Elementos do DOM
 const resetForm = document.getElementById('reset-form');
